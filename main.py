edbc0ef2ddffac0e16afe113737cccd7fb5fedb5
IndoorGladiator <boot.dev@sungcad.com>
2025-05-05 17:26:34 +0000

    Added letter counting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import *
 
 def get_book_text(book_filepath):
     return open(book_filepath).read()
@@ -7,5 +7,7 @@ def main():
     book = get_book_text("books/frankenstein.txt")
     words = count_words(book)
     print(f"{words} words found in the document")
+    letters = count_letters(book)
+    print(letters)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,10 @@
 
 def count_words(text):
     return len(text.split())
+
+def count_letters(text):
+    letter_set = set(text.lower())
+    letter_map = dict.fromkeys(letter_set, 0)
+    for letter in text.lower():
+        letter_map[letter] += 1
+    return letter_map
